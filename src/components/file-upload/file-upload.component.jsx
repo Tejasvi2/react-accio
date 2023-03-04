7aaee0cbe7a79f9ea0bea9a6af078df079145797
tejasvi <tejshinde9876@gmail.com>
2023-03-04 10:53:07 +0530

    fix: upload page fixes added

--- src/components/file-upload/file-upload.component.jsx
+++ src/components/file-upload/file-upload.component.jsx
@@ -18,6 +18,7 @@ import './file-upload.scss';
 
 const FileUpload = () => {
   const navigate = useNavigate();
+  const hiddenFileInput = React.useRef(null);
   
   
   // for onchange event
@@ -81,16 +82,40 @@ const FileUpload = () => {
     navigate(`/chat/${id}`, { state: viewPdf } )
   }
 
+  
+  const handleClick = event => {
+    hiddenFileInput.current.click();
+  };
+  const handleChange = event => {
+    const fileUploaded = event.target.files[0];
+    console.log({fileUploaded})
+    if(fileUploaded){
+      if(fileUploaded&&fileType.includes(fileUploaded.type)){
+        let reader = new FileReader();
+            reader.readAsDataURL(fileUploaded);
+            reader.onloadend = (e) =>{
+              setPdfFile(e.target.result);
+              setPdfFileError('');
+            }
+          console.log('herererere')
+
+      }
+      else{
+        setPdfFile(null);
+        setPdfFileError('Please select valid pdf file');
+      }
+    }
+    else{
+      console.log('select your file');
+    }
+    // props.handleFile(fileUploaded);
+  };
+
 
   return (
-    <div className='container'>
-    <br></br> 
-    {/* <button onClick={chatPage}>chat</button> */}
-      <form className='form-group' onSubmit={handlePdfFileSubmit}>
+    <div className='file-container'>
+      {/* <form className='form-group' onSubmit={handlePdfFileSubmit}>
        <div className='upload-wrapper'>
-
-        {/* 
-         */}
         <input type="file" className='input-file'
           required onChange={handlePdfFileChange}
         />
@@ -100,7 +125,24 @@ const FileUpload = () => {
         </button>
         </div>
       </form>
-      <br></br>
+      <br></br> */}
+      <form className='form-group' onSubmit={handlePdfFileSubmit}>
+        <h4>Upload a file and see the magic</h4>
+      <div className='upload-container'>
+        <div className='upload-icon'> <img src={require('../../assets/icons/upload.png')} className="upload-logo" /></div>
+        <div className='primary-text'>Select a file or drag and drop here</div>
+        <div className='secondary-text'>PDF file size no more than 10MB</div>
+      <button className='btn btn-success btn-md upload-btn' onClick={handleClick}>
+        Upload
+      </button>
+      </div>
+      </form>
+    
+      <input type="file"
+             ref={hiddenFileInput}
+             onChange={handleChange}
+             style={{display:'none'}} 
+      /> 
       <h4>View PDF</h4>
           <div className='pdf-container'>
               {/* show pdf conditionally (if we have one)  */}
@@ -138,6 +180,7 @@ const FileUpload = () => {
                           </table>
                       </div>
                   </div>
+                  
       </>}
 
       {/* if we dont have pdf or viewPdf state is null */}
